617aa81207de883740c8aab41a24f8e740277e0a
zzap <eat.yummies@gmail.com>
2017-08-10 22:59:29 +0300

    Change style import

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,5 @@
+import '@/index.styl'
 import hi from '~/hello'
-import allStyles from '@/index.styl'
 
-const hello = '<h1>Hello world</h1>'
+const hello = '<h1>Hello, world!</h1>'
 hi(hello)
-// include styles at the end
-allStyles.use()
